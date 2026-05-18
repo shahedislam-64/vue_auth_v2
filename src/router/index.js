be7cf55209f72f29f29e2a952c0794ac0e8eb29d
shahed-64 <shahedislam6475@gmail.com>
2026-05-18 07:25:49 +0600

    fix vue router login redirect issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,22 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-
-    // Guest Routes
-    {
-      path: '/register',
-      name: 'registerView',
-      component: () => import('../views/registerView.vue'),
-      meta: { guest: true },
+      redirect: '/login',
     },
 
     {
@@ -32,7 +16,19 @@ const router = createRouter({
       meta: { guest: true },
     },
 
-    // Protected Routes
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+
+    {
+      path: '/register',
+      name: 'registerView',
+      component: () => import('../views/registerView.vue'),
+      meta: { guest: true },
+    },
+
     {
       path: '/dashboard',
       name: 'dashboardView',
