f313a6e1c81352bcc717e664f917742303951f47
shahed-64 <shahedislam6475@gmail.com>
2026-05-22 21:30:40 +0600

    ghurar dim korsi

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://laravel-auth-backend-64.onrender.com/api',
+  baseURL: 'http://127.0.0.1:8000/api',
 })
 
 const token = localStorage.getItem('token')
