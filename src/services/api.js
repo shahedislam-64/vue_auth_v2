5f0f85d511d5d44faab1f539a3fcd79d8c129c24
shahed-64 <shahedislam6475@gmail.com>
2026-05-24 13:30:13 +0600

    api connection

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://laravel-auth-backend-64.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
-const token = localStorage.getItem('token')
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
 
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
 
 export default api
