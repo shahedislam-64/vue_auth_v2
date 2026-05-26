f172a94f22c8619d8b72b12ebd84d69cb378d1ee
shahedislam-64 <shahedislam17537875@gmail.com>
2026-05-26 07:22:53 +0600

    fixing and fixing

--- src/services/api.js
+++ src/services/api.js
@@ -4,10 +4,4 @@ const api = axios.create({
   baseURL: 'https://laravel-auth-backend-65.onrender.com/api',
 })
 
-const token = localStorage.getItem('token')
-
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
-
 export default api
